b512be636c9a1d4100029421df6f553a2578c1bc
ahmedmhb <ahmedmhbpnus@gamil.com>
2024-12-15 20:10:59 +0000

    Add Copy of Untitled0.py to TP3 directory

---
+++ TP3/Copy of Untitled0.py
@@ -0,0 +1,9 @@
+class chien:
+  def __init__(self,nom,race,age):
+    self.nom=nom
+    self.race=race
+    self.age=age
+  def aboyer(self):
+    return "aboyer"
+chien1=chien("rex","pitbull",3)
+chien1.aboyer()
