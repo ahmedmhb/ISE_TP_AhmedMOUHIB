086668f0d6c50d153f66f56be2ea6ef14f506361
ahmedmhb <89659307+ahmedmhb@users.noreply.github.com>
2024-12-16 01:32:55 +0100

    Delete TP3 directory

--- TP3/Copy of Untitled0.py
+++
@@ -1,9 +0,0 @@
-class chien:
-  def __init__(self,nom,race,age):
-    self.nom=nom
-    self.race=race
-    self.age=age
-  def aboyer(self):
-    return "aboyer"
-chien1=chien("rex","pitbull",3)
-chien1.aboyer()
